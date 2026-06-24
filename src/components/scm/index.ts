ff51d2beffb4e97c3c3a5e56a0c8d0012d65d0be
Chu Quang Thang <chuquangthangftu@gmail.com>
2026-06-24 22:23:14 +0700

    clean: remove old layout, sections, scm components, and resume data

--- src/components/scm/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Hero } from './Hero';
-export { default as Problem } from './Problem';
-export { default as Solution } from './Solution';
-export { default as Features } from './Features';
